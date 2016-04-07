0da5b2d0d6c098960b1401a40c65858dfd4b6628
Brandon Okert <brandon.okert@hootsuite.com>
2016-04-07 10:02:19 -0700

    Added FromJson function

--- model/json.go
+++ model/json.go
@@ -3,12 +3,13 @@ package model
 import (
 	"encoding/json"
 	"errors"
+	"io"
 )
 
 // The default marshaler for ToJson. If you don't know what to pass it, just pass the default
 var DefaultMarshaler = json.Marshal
 
-// Coverts a struct (really any interface pointer) into a json string
+// Coverts a struct into a json string
 // Requires a function that creates a new encoder for the object. Use model.DefaultEncoder in general
 func ToJson(marshal func(interface{}) ([]byte, error), object interface{}) (string, error) {
 	result, err := marshal(object)
@@ -16,4 +17,13 @@ func ToJson(marshal func(interface{}) ([]byte, error), object interface{}) (stri
 		return "", errors.New("Failed to json encode object: " + err.Error())
 	}
 	return string(result), nil
+}
+
+func FromJson (content io.Reader, model interface{}) error {
+	decoder := json.NewDecoder(content)
+	err := decoder.Decode(model)
+	if err != nil {
+		return errors.New("Failed to json decode input: " + err.Error())
+	}
+	return nil
 }
--- model/json_test.go
+++ model/json_test.go
@@ -5,12 +5,23 @@ import (
 	"github.com/bcokert/render-cloud/model"
 	"github.com/bcokert/render-cloud/testutils"
 	"testing"
+	"bytes"
+	"fmt"
 )
 
 type TestModel struct {
 	Arr []int
 	Str string
 	Mod *TestModel
+}
+
+type TestModel2 struct {
+	Int int
+	Str string
+}
+
+func (model TestModel2) String() string {
+	return fmt.Sprintf("{Int: %d, Str: %s}", model.Int, model.Str)
 }
 
 func TestToJsonSuccess(t *testing.T) {
@@ -33,5 +44,39 @@ func TestToJsonError(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("ToJson did not return an error when the underlying Encode returned an error")
+	}
+}
+
+func TestFromJsonSuccess(t *testing.T) {
+	var testModel TestModel2
+	jsonString := "{\"Int\": 66, \"Str\": \"Hello\"}"
+	data := bytes.NewReader([]byte(jsonString))
+
+	err := model.FromJson(data, &testModel)
+
+	if err != nil {
+		t.Errorf("FromJson failed to decode a json string %s", jsonString)
+	}
+
+	expectedModel := TestModel2{66, "Hello"}
+	if testModel != expectedModel {
+		t.Errorf("FromJson decoded a model improperly. Received %s, but expected %s", testModel, expectedModel)
+	}
+}
+
+func TestFromJsonErrorDecoding(t *testing.T) {
+	var testModel TestModel2
+	jsonString := "\\fd89ha"
+	data := bytes.NewReader([]byte(jsonString))
+
+	err := model.FromJson(data, &testModel)
+
+	if err == nil {
+		t.Errorf("FromJson did not fail when given an illegal string")
+	}
+
+	expectedError := "Failed to json decode input: invalid character '\\\\' looking for beginning of value"
+	if err.Error() != expectedError {
+		t.Errorf("FromJson did not return the correct error message when decoding an illegal string. Expected %s, received %s", expectedError, err.Error())
 	}
 }
