061ae4acaea8d3929a8c2a4b93399c7aec5d58bc
Brandon Okert <brandon.okert@hootsuite.com>
2016-04-09 16:46:10 -0700

    Added screen information to camera

--- model/camera.go
+++ model/camera.go
@@ -5,7 +5,10 @@ import (
 )
 
 type Camera struct {
-	Origin    *mgl64.Vec3 `json:"origin,omitempty"`
-	Direction *mgl64.Vec3 `json:"direction,omitempty"`
-	Up        *mgl64.Vec3 `json:"up,omitempty"`
+	Origin         *mgl64.Vec3 `json:"origin,omitempty"`
+	Direction      *mgl64.Vec3 `json:"direction,omitempty"`
+	Up             *mgl64.Vec3 `json:"up,omitempty"`
+	ScreenWidth    *float64    `json:"screenWidth,omitempty"`
+	ScreenHeight   *float64    `json:"screenHeight,omitempty"`
+	ScreenDistance *float64    `json:"screenDistance,omitempty"`
 }
--- model/camera_test.go
+++ model/camera_test.go
@@ -5,12 +5,20 @@ import (
 	"github.com/bcokert/render-cloud/testutils"
 	"testing"
 	"github.com/go-gl/mathgl/mgl64"
+	"github.com/bcokert/render-cloud/utils"
 )
 
 func TestCameraJsonEncodes(t *testing.T) {
-	camera := model.Camera{Origin: &mgl64.Vec3{10, 10, -50}, Direction: &mgl64.Vec3{0, 0, 1}, Up: &mgl64.Vec3{0, 1, 0}}
+	camera := model.Camera{
+		Origin: &mgl64.Vec3{10, 10, -50},
+		Direction: &mgl64.Vec3{0, 0, 1},
+		Up: &mgl64.Vec3{0, 1, 0},
+		ScreenWidth: utils.FloatPointer(4),
+		ScreenHeight: utils.FloatPointer(3),
+		ScreenDistance: utils.FloatPointer(1),
+	}
 
-	expectedJson := "{\"origin\":[10,10,-50],\"direction\":[0,0,1],\"up\":[0,1,0]}"
+	expectedJson := "{\"origin\":[10,10,-50],\"direction\":[0,0,1],\"up\":[0,1,0],\"screenWidth\":4,\"screenHeight\":3,\"screenDistance\":1}"
 
 	testutils.ExpectJsonEncoding(t, &camera, expectedJson)
 }
